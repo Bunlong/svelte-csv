a92f2eec7cd0e5a13c75d7310ffedc89c1f7736c
Bunlong <bunlong.van@gmail.com>
2021-05-05 23:59:13 +0700

    :heavy_plus_sign: readRemoteFile

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { readString } from './readString.svelte';
 export { jsonToCSV } from './jsonToCSV.svelte';
+export { readRemoteFile } from './readRemoteFile.svelte';
---
+++ src/readRemoteFile.svelte
@@ -0,0 +1,7 @@
+<script context='module'>
+  import PapaParse from 'papaparse';
+
+  export function readRemoteFile(url, options) {
+    PapaParse.parse(url, Object.assign({}, { download: true }, options));
+  }
+</script>
