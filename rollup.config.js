4c518d33115247fc5e1fc8367aae2c235a722615
Bunlong <bunlong.van@gmail.com>
2022-02-10 00:41:27 +0700

    Fix not found module

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
 import pkg from './package.json';
 
 const name = pkg.name
@@ -9,14 +10,29 @@ const name = pkg.name
   .replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-  input: 'src/index.js',
+  input: 'src/svelte-csv.js',
   output: [
-    { file: pkg.module, 'format': 'es' },
-    { file: pkg.main, 'format': 'umd', name }
+    // { file: pkg.module, 'format': 'esm', name },
+    // { file: pkg.main, 'format': 'umd', name },
+    // { file: pkg.main, format: 'cjs', name },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      exports: 'named',
+    },
   ],
+  external: ['svelte', 'papaparse'],
   plugins: [
-    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
     commonjs(),
-    svelte()
+    svelte(),
+    terser()
   ]
 };
